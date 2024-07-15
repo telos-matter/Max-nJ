8f8670d8ccfa285403ac3b88ea6acf967d73f79c
HEMMOUDA Aymane <ahemmouda@gmail.com>
2024-07-16 00:53:42 +0200

    FEAT: add getID in Player.java

--- src/main/java/telosmatter/maxnj/Player.java
+++ src/main/java/telosmatter/maxnj/Player.java
@@ -1,4 +1,12 @@
 package telosmatter.maxnj;
 
 public interface Player {
+
+    /**
+     * A way for you to identify this player
+     * from the others in the evaluation
+     * method.
+     */
+    public abstract int getID ();
+
 }
