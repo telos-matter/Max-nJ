f914746d4ec5816458ad3fcb5849038124fcfa99
HEMMOUDA Aymane <ahemmouda@gmail.com>
2024-07-16 01:02:20 +0200

    MODIFY: GameState takes Generics for player and move

--- src/main/java/telosmatter/maxnj/GameState.java
+++ src/main/java/telosmatter/maxnj/GameState.java
@@ -3,36 +3,39 @@ package telosmatter.maxnj;
 import java.util.Collection;
 
 /**
- * The state of a game. A node
+ * A state of a game that is
+ * played by <code>P</code> type
+ * of Players, through <code>M</code>
+ * type of Moves.
+ * Represents a node
  * in the game tree.
  */
-public interface GameState {
+public interface GameState <P, M> {
 
     /**
      * Give me the player that will play in
      * this current game state. The one
-     * who will make a move.
+     * who will make a move. The player
+     * whose turn is it.
      */
-    public abstract Player getPlayer ();
+    public abstract P getPlayer ();
 
     /**
      * Give me all the possible moves that can
      * be taken in this current game state
      */
-    public abstract Collection <Move> getPossibleMoves ();
+    public abstract Collection <M> getPossibleMoves ();
 
     /**
      * Give me the game state that
      * will result from playing this move
      * in this current game state
      */
-    public abstract GameState playMove (Move move);
+    public abstract GameState <P, M> playMove (M move);
 
     /**
      * Is the game over in this state?
      */
     public abstract boolean isGameOver ();
-
-
 
 }
