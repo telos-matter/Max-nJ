0f80abd7ca11ca2f8e762383ce433ffe12215dff
HEMMOUDA Aymane <ahemmouda@gmail.com>
2024-07-16 20:01:09 +0200

    FEAT: getPlayers in GameState.java
    + DOCS

--- src/main/java/telosmatter/maxnj/GameState.java
+++ src/main/java/telosmatter/maxnj/GameState.java
@@ -3,15 +3,25 @@ package telosmatter.maxnj;
 import java.util.Collection;
 
 /**
- * A state of a <code>G</code>
- * type of Game, that is
+ * A state of a "<code>G</code>
+ * type of Game", that is
  * played by <code>P</code> type
  * of Players, through <code>M</code>
  * type of Moves.
  * Represents a node
  * in the game tree.
+ *
+ * @param <P> Player type
+ * @param <M> Move type
+ * @param <G> The GameState implementation
  */
 public interface GameState <P, M, G extends GameState <P, M, G>> {
+
+    /**
+     * Give me all the players that are playing
+     * in this game.
+     */
+    public abstract Collection <P> getPlayers ();
 
     /**
      * Give me the player that will play in
@@ -23,14 +33,17 @@ public interface GameState <P, M, G extends GameState <P, M, G>> {
 
     /**
      * Give me all the possible moves that can
-     * be taken in this current game state
+     * be taken in this current game state.
      */
     public abstract Collection <M> getPossibleMoves ();
 
     /**
      * Give me the game state that
-     * will result from playing this move
-     * in this current game state
+     * will result if this move was
+     * played.
+     * This should not modify your actual
+     * game, you should rather make a copy
+     * of the game and play on the copy.
      */
     public abstract G playMove (M move);
 
